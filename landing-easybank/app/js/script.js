1d21a162e2084a8a8c9451471f2e10771ed46edd
Joshua Hellard <67675776+jhellard@users.noreply.github.com>
2021-04-23 15:20:21 -0400

    First commit

---
+++ landing-easybank/app/js/script.js
@@ -0,0 +1,25 @@
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector('body');
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", () => {
+  if (header.classList.contains("open")) {
+    // Close
+    body.classList.remove('noscroll');
+    header.classList.remove("open");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } else {
+    // Open
+    body.classList.add('noscroll');
+    header.classList.add("open");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
+});
